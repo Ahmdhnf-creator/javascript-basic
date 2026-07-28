5452ba183375bd4f1b83c38caafc46f0cef15919
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-28 10:29:55 +0700

    biodata

---
+++ bab-03-variable/latihan/biodata.js
@@ -0,0 +1,16 @@
+/*Nama : Budi Santoso
+Umur : 20
+Kota : Yogyakarta
+Pekerjaan : Mahasiswa*/
+
+//variabel
+const namaLengkap ="Budi Santoso"
+let umur ="20"
+const kota ="Yogyakarta"
+let pekerjaan ="Mahasiswa"
+
+//console.log
+console.log(namaLengkap)
+console.log(umur)
+console.log(kota)
+console.log(pekerjaan)
