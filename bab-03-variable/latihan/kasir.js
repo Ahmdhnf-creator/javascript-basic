7ecaf5e5e032a29a1713d1687f181a740bd4ff8a
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-28 14:14:59 +0700

    kasir

---
+++ bab-03-variable/latihan/kasir.js
@@ -0,0 +1,21 @@
+/*
+Nama Produk     : Sepatu Sneaker Speedcat Ferrari Unisex
+Harga Satuan    : Rp 1.999.000
+Jumlah Dibeli   : 6pcs
+PPN             : 5%
+*/
+
+//variabel
+const namaProduk ="Sepatu Sneaker Speedcat Ferrari Unisex";
+const hargaSatuan = 1999000;
+const ppn = 5/100;
+let jumlahDibeli = 6;
+let subtotal = hargaSatuan * jumlahDibeli;
+let totalBayar = ((hargaSatuan*jumlahDibeli)*(5/100))
+
+//console.log
+console.log(namaProduk)
+console.log(hargaSatuan)
+console.log(jumlahDibeli)
+console.log(subtotal=hargaSatuan*jumlahDibeli)
+console.log(totalBayar=((hargaSatuan*jumlahDibeli)*(5/100)))
---
+++ bab-03-variable/latihan/ratarata.js
@@ -0,0 +1,16 @@
+/*
+nilai mtk   : 98
+nilai IPA   : 100
+nilai Bindo : 80 */
+
+//variabel
+let nilaiMatematika = 98
+let nilaiIpa = 100
+let nilaiBindo = 80
+let nilaiRatarata = (nilaiMatematika + nilaiIpa + nilaiBindo)/3
+
+//consol.log
+console.log(nilaiMatematika)
+console.log(nilaiIpa)
+console.log(nilaiBindo)
+console.log(nilaiRatarata = (nilaiMatematika + nilaiIpa + nilaiBindo)/3)
