2e388dbadd4f38dc167e622c1a7b9eda058b9e7e
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-28 13:41:50 +0700

    total belanja

---
+++ bab-03-variable/latihan/belanja.js
@@ -0,0 +1,17 @@
+/*
+Produk  : Sepatu Sneaker Speedcat McLAREN RACING Unisex
+Harga   : 1.999.000
+Jumlah  : 2
+Total   : harga*jumlah */
+
+//variabel
+const produk ="Sepatu Sneaker Speedcat McLAREN RACING Unisex";
+let harga = 1999000;
+let jumlah = 2;
+let total ="harga*jumlah";
+
+//consolelog
+console.log(produk)
+console.log(harga)
+console.log(jumlah)
+console.log(total= harga*jumlah)
