1d0edd520e85f3b11a6d39ed2ddad3a5649c7f67
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-28 14:26:51 +0700

    Profil Pengguna

---
+++ bab-03-variable/latihan/profilpengguna.js
@@ -0,0 +1,23 @@
+/*
+Nama        : Budiono Siregar
+Umur        : 25 Tahun
+Kota        : Yogyakarta
+Email       : budionosiregar77@gmail.com
+Hobi        : Tenis
+Cita-cita   : Freelance Enterprise consultant*/
+
+//variabel
+const nama = "Budiono Siregar"
+let umur = 25
+const kota = "Yogyakarta"
+const email = "budionosiregar77@gmail.com"
+const hobi = "Tenis"
+const citaCita = "Freelance Enterprise consultant"
+
+//console.log
+console.log(nama)
+console.log(umur)
+console.log(kota)
+console.log(email)
+console.log(hobi)
+console.log(citaCita)
