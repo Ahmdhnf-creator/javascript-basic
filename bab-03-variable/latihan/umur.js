8aeb02c1c0a0c06f516ba6996c9dfd1d2219be77
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-28 13:49:44 +0700

    umur

---
+++ bab-03-variable/latihan/umur.js
@@ -0,0 +1,17 @@
+/*
+Nama    : Budi
+Lahir   : 1890
+*/
+
+//variabel
+const nama = "Budi"
+const tahunLahir = 1890
+let tahunSekarang = 2026
+let umur = "tahunSekarang-tahunLahir"
+
+//consol.log
+console.log(nama)
+console.log(tahunLahir)
+console.log(tahunSekarang)
+console.log(umur = tahunSekarang-tahunLahir)
+
