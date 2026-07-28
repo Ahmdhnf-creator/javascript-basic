a3ee59cbad3290391682528903efa7eb5119b73c
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-28 13:19:46 +0700

    nilai variabel

---
+++ bab-03-variable/latihan/nilaivar.js
@@ -0,0 +1,13 @@
+
+// saldo awal : 100000
+// saldo tambahan : 50000
+// saldo akhir : 150000 
+
+//variabel
+let saldoAwal = 100000
+let saldoTambahan = 50000
+
+//console.log
+console.log(saldoAwal);
+console.log(saldoTambahan);
+console.log(saldoAwal + saldoTambahan)
