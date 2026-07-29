72ce73141cf644afd2cbe9d402acfec3a892350a
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-29 11:12:00 +0700

    tugas typeof

---
+++ bab-04-tipedata/latihan/lat4.js
@@ -0,0 +1,23 @@
+//variabel
+const nama = "Budiono Siregar"
+let umur = 25
+let sudahLulus = true
+let alamat = null
+let nomorTelepon;
+const hobi = ["berenang", "membaca", "berkuda", "memanah", "melukis"];
+let profil = {
+    nama:"Budiono Siregar",
+    umur:25,
+    sekolah:"SMK indah Jakarta",
+    jurusan:"TKJ",
+    status:"aktif"
+}
+
+//mengecek tipe data
+console.log(typeof nama)
+console.log(typeof umur)
+console.log(typeof sudahLulus)
+console.log(typeof alamat)
+console.log(typeof nomorTelepon)
+console.log(typeof hobi)
+console.log(typeof profil)
