e8f410ac2e844ac6c3aa94d509989f78cdb98997
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-29 11:03:51 +0700

    tugas object

---
+++ bab-04-tipedata/latihan/lat3.js
@@ -0,0 +1,15 @@
+//variabel
+let profil = {
+    nama:"Budiono Siregar",
+    umur:25,
+    sekolah:"SMK indah Jakarta",
+    jurusan:"TKJ",
+    status:"aktif"
+}
+
+//console.log
+console.log(profil.nama)
+console.log(profil.umur)
+console.log(profil.sekolah)
+console.log(profil.jurusan)
+console.log(profil.status)
