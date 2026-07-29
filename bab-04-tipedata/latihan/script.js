25259f427009f0e2da39f0c6ee80b23e23cd2957
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-29 10:43:34 +0700

    tugas primitif

---
+++ bab-04-tipedata/latihan/script.js
@@ -0,0 +1,13 @@
+//variabel
+const nama = "Budiono Siregar"
+let umur = 25
+let sudahLulus = true
+let alamat = null
+let nomorTelepon;
+
+//console.log
+console.log(nama)
+console.log(umur)
+console.log(sudahLulus)
+console.log(alamat)
+console.log(nomorTelepon)
