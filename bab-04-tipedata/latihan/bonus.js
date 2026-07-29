b6ab56e07eeff1d9bbaca7fce82cdd0d92e2b4f8
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-29 11:43:21 +0700

    bonus

---
+++ bab-04-tipedata/latihan/bonus.js
@@ -0,0 +1,13 @@
+let kelas = {
+    namaKelas : "Anggrek Pontianak",
+    jumlahSiswa : 987,
+    ketuaKelas : "Ambarukman",
+    mapelPelajaran : ["IPS","IPA","MTK","Kekebalan Tubuh"]
+}
+
+//console.log
+console.log(kelas.namaKelas)
+console.log(kelas.jumlahSiswa)
+console.log(kelas.ketuaKelas)
+console.log(kelas.mapelPelajaran[0])
+console.log(kelas.mapelPelajaran[3])
