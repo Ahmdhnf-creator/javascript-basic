927de15eddaca83d06ff9b5763c64e3fa16387a8
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-29 11:26:47 +0700

    analisis program

---
+++ bab-04-tipedata/latihan/lat6.js
@@ -0,0 +1,28 @@
+const tahun = "2026";
+const umur = 17;
+const aktif = true;
+
+console.log(typeof tahun);
+console.log(typeof umur);
+console.log(typeof aktif);
+
+console.log(tahun + 1);
+console.log(umur + 1);
+
+//soal
+/*
+1. Apa tipe data variabel tahun?
+2. Mengapa hasil tahun + 1 bukan menjadi 2027?
+3. Apa tipe data variabel umur?
+4. Berapa hasil umur + 1?
+5. Apa fungsi operator typeof?
+*/
+
+//jawaban
+/*
+ 1. string
+ 2. karena "2026" bertipe string, jadi sistem menganggap itu adalah kata
+ 3. number
+ 4. 18, karena 17 bertipe number
+ 5. untuk melihat tipe data
+*/
