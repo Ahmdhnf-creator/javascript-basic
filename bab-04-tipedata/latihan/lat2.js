efaadf0e13ad51ff392e29afa527278e29f620b6
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-29 10:51:37 +0700

    tugas array

---
+++ bab-04-tipedata/latihan/lat2.js
@@ -0,0 +1,11 @@
+//variabel
+const hobi = ["berenang", "membaca", "berkuda", "memanah", "melukis"];
+
+//menampilkan hobi pertama
+console.log(hobi[0])
+
+//menampilkan hobi ke tiga
+console.log(hobi[2])
+
+//menampilkan hobi terakhir
+console.log(hobi[4])
