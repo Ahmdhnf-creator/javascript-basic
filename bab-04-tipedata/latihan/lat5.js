32cb378739a14e0ee899eaff1094bf4073d5bba8
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-29 11:18:25 +0700

    tugas tebak tipe

---
+++ bab-04-tipedata/latihan/lat5.js
@@ -0,0 +1,9 @@
+console.log(typeof "Hello"); //string
+console.log(typeof 100); //number
+console.log(typeof true); //boolean
+console.log(typeof null); //null
+console.log(typeof []); //array
+console.log(typeof {}); //object
+console.log(typeof undefined); //undefined
+
+//tebakan salah pada null dan array tapi jawaban saya sesuai materi
