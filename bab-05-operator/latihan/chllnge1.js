6b361c9073cc57248facd9548d2de4da9eeea009
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-30 13:17:20 +0700

    Challenge 1

---
+++ bab-05-operator/latihan/chllnge1.js
@@ -0,0 +1,8 @@
+const tugas = 80;
+const uts = 75;
+const uas = 90;
+let nilaiAkhir = (tugas+uts+uas)/3
+let lulus = nilaiAkhir >= 75
+
+console.log("nilai akhir =",nilaiAkhir)
+console.log("lulus =",lulus)
