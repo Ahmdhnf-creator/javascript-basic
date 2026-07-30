eee22d81d1cc0eb9576fdb9d5f29dab5e9fce064
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-30 11:38:29 +0700

    Challenge Mini Kasir

---
+++ bab-05-operator/latihan/soal14.js
@@ -0,0 +1,9 @@
+const hargaBuku = 45000;
+const jumlahBeli = 4;
+const diskon = 15000;
+let subtotal = hargaBuku*jumlahBeli
+let total = subtotal-diskon
+
+//output
+console.log("subtotal =",subtotal)//output subtotal
+console.log("total =",total)//output total
