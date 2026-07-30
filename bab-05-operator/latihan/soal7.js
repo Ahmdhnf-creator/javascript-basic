d52529b540a1e71d6d3ee1edf7db1cefb393488b
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-30 10:56:37 +0700

    == vs ===

---
+++ bab-05-operator/latihan/soal7.js
@@ -0,0 +1,6 @@
+console.log(10 == "10");//true karena tidak membandingkan tipe
+console.log(10 === "10");//false karena salah satu string 
+console.log(false == 0);//true angka 0 di konverensi mnjadi false
+console.log(false === 0);//false karena boolean dan number tidak sama
+console.log(null == undefined);//true, sama sama nilai kosong
+console.log(null === undefined);//false karena beda tipe
