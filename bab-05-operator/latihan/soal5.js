c86a259e835301167d2229e1aabb02985b7a52e9
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-30 10:47:35 +0700

    Studi Kasus Kasir

---
+++ bab-05-operator/latihan/soal5.js
@@ -0,0 +1,9 @@
+const harga = 18000;
+const jumlah = 6;
+const diskon = 10000;
+let subtotal = harga*jumlah
+let total = subtotal-diskon
+
+//console.log
+console.log("subtotal =",subtotal)//subtotal
+console.log("total =",total)//total
