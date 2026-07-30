c769b89851beaf6b28cb1a7818d869014b82c079
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-30 11:22:14 +0700

    prioritas

---
+++ bab-05-operator/latihan/soal10.js
@@ -0,0 +1,5 @@
+console.log(
+  8 + 4 * 2,
+); /*4*2=8, 8+8=16, jadi hasil akhir. hal ini disebabkan perkalian lebih diutamakan dibanding penjumlahan*/
+console.log((8 + 4) * 2); //8+4=12, 12*2=24, jadi hasil akhirnya 24. hal ini disebabkan karena tanda kurung lebih prioritas dibanding perkalian*/
+console.log(20 / 5 + 3 * 2); //20/5=4, 3*2=6, 4+6=10, jadi hasil akhirnya 10. hal ini disebabkan karena perkalian dan pembagian lebih diutamakan dibanding penjumlahan.*/
