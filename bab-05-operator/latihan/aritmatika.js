2e522160807468e99120bb63701814edfa08a8e7
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-30 10:10:21 +0700

    tugas aritmatika

---
+++ bab-05-operator/latihan/aritmatika.js
@@ -0,0 +1,9 @@
+//variabel
+const panjang = 15;
+const lebar = 8;
+let luas = panjang*lebar;
+let keliling = panjang+panjang+lebar+lebar;
+
+//console.log
+console.log("luas",luas);
+console.log("keliling",keliling);
