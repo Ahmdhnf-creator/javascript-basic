bc6a9248baf28710841cab4d08e944c9ffcb4f1c
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-30 11:34:49 +0700

    Studi Kasus Seleksi Peserta

---
+++ bab-05-operator/latihan/soal13.js
@@ -0,0 +1,12 @@
+/*
+umur minimal 17 tahun
+memiliki KTP
+sudah mendaftar 
+*/
+let umur = 20;
+let punyaKTP = true;
+let sudahDaftar = true;
+let lolos = umur>=17 && punyaKTP && sudahDaftar
+
+//output
+console.log(lolos)
