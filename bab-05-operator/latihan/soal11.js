dcd0e765b74a50c1bab3c637adfe4c76974ec5d3
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-30 11:26:31 +0700

    debugging

---
+++ bab-05-operator/latihan/soal11.js
@@ -0,0 +1,11 @@
+let angka = 20;
+
+angka += 5;
+
+console.log(angka);
+
+/*
+Apa output program tersebut? hasilnya tetep 20
+Mengapa hasilnya demikian? karena tidak menyimpan
+Perbaiki program agar menghasilkan: dengan ditambahkan "="
+ */
