9286c32e374e00640472136947ba37e65dcbaf6c
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-31 09:52:33 +0700

    hari dg switch

---
+++ bab-6-percabangan/latihan/soal4.js
@@ -0,0 +1,25 @@
+let hariKe = 5;
+
+switch (hariKe) {
+  case 1:
+    console.log("Senin");
+    break;
+  case 2:
+    console.log("Selasa");
+    break;
+  case 3:
+    console.log("Rabu");
+    break;
+  case 4:
+    console.log("Kamis");
+    break;
+  case 5:
+    console.log("Jumat");
+    break;
+  case 6:
+    console.log("Sabtu");
+    break;
+  case 7:
+    console.log("Minggu");
+    break;
+}
