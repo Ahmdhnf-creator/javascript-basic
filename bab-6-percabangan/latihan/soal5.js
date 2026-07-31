29e3d18487b7753a3732f40953af1e5c18f6f62a
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-31 10:07:03 +0700

    menu minuman

---
+++ bab-6-percabangan/latihan/soal5.js
@@ -0,0 +1,18 @@
+let menu = "teh";
+
+switch (menu) {
+  case "kopi":
+    console.log("Harga : 15.000");
+    break;
+
+  case "teh":
+    console.log("Harga : 10.000");
+    break;
+
+  case "jus":
+    console.log("Harga : 18.000");
+    break;
+
+  default:
+    console.log("Menu tidak tersedia");
+}
