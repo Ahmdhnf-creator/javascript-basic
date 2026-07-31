8d67072f284c2eaeadb2a8c34f99f7f074365eb1
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-31 10:28:56 +0700

    validasi lojin

---
+++ bab-6-percabangan/latihan/soal9.js
@@ -0,0 +1,9 @@
+let username = "budi";
+let password = "";
+
+if (!username || !password){
+    console.log("Data belum lengkap")//salah satu atau ke duanya kosong maka output "data blm lengkap"
+}
+else{
+    console.log("Login Berhasil")//jika keduanya terisi maka output "lojin berhasil"
+}
