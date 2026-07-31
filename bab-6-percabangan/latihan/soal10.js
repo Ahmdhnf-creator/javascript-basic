bbbfbbdae83749ecade0a01439bf801c55234362
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-31 10:39:19 +0700

    lampu merah (if else if)

---
+++ bab-6-percabangan/latihan/soal10.js
@@ -0,0 +1,11 @@
+let lampu = "merah";
+
+if (lampu === "merah") {
+  console.log("Berhenti");
+} else if (lampu === "kuning") {
+  console.log("Bersiap");
+} else if (lampu === "hijau") {
+  console.log("Jalan");
+} else {
+  console.log("Warna lampu tidak dikenali.");
+}
