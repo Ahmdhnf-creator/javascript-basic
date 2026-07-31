ad3285bdfa619a17d28fcbd9376c74be2d8db6e4
Ahmdhnf <hanifabqari77@gmail.com>
2026-07-31 13:22:53 +0700

    Challenge Nilai Akhir

---
+++ bab-6-percabangan/latihan/soal13.js
@@ -0,0 +1,14 @@
+let nilai = 76;
+
+if (nilai >=90){
+    console.log("Sangat Baik")
+}
+else if (nilai >=75){
+    console.log("Baik")
+}
+else if (nilai >=60){
+    console.log("Baik")
+}
+else {
+    console.log("Perlu Belajar Lagi")
+}
