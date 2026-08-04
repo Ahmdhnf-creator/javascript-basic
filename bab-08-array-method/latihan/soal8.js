3db231c80148974b8b86cf947ea532ce37ef4400
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-04 11:20:03 +0700

    mencari posisi

---
+++ bab-08-array-method/latihan/soal8.js
@@ -0,0 +1,9 @@
+let kota = [
+  "Jakarta",
+  "Bandung",
+  "Yogyakarta",
+  "Surabaya",
+];
+console.log(kota.indexOf("Bandung"))
+console.log(kota.indexOf("Surabaya"))
+console.log(kota.indexOf("Bali"))
