70914e705cdfe9449eb076673f4bb14be78bf43b
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-05 15:57:07 +0700

    12

---
+++ bab-09-fungsi/latihan/soal12.js
@@ -0,0 +1,20 @@
+function tambah(a,b){
+    return a+b;
+}
+
+function kurang(a,b){
+    return a-b;
+}
+
+function kali(a,b){
+    return a*b;
+}
+
+function bagi(a,b){
+    return a/b;
+}
+
+console.log(tambah(1,2))
+console.log(kurang(6,5))
+console.log(kali(2,4))
+console.log(bagi(4,2))
