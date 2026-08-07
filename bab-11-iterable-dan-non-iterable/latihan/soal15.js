b87d1b393357a3faae64fc42d381ec9919b212c7
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-07 14:18:16 +0700

    mencari nilai terbesar pada array

--- bab-11-iterable-dan-non-iterable/latihan/soal15.js
+++ bab-11-iterable-dan-non-iterable/latihan/soal15.js
@@ -1,1 +1,11 @@
 const nilai = [75, 90, 88, 100, 67, 95];
+
+let terbesar = nilai[0];
+
+for (let item of nilai) {
+    if (item > terbesar) {
+        terbesar = item;
+    }
+}
+
+console.log("Nilai terbesar:", terbesar);
