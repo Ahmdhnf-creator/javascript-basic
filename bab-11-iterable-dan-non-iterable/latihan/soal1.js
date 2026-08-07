e879e642c403efa0305cabc0bec9539bea027050
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-07 10:18:12 +0700

    menentukan iterable atau non

---
+++ bab-11-iterable-dan-non-iterable/latihan/soal1.js
@@ -0,0 +1,5 @@
+"Indonesia"//Iterable karena string
+["Merah", "Putih"]//Iterable karena array
+150//Non Iterable karena number
+true//Non Iterable karena boolean
+{ nama: "Budi" }//Non Iterable karena object
