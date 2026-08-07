8faecb67f74eb1d59a0e99f03aa6f1a3f3dc2ea7
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-07 11:04:15 +0700

    biodata

---
+++ bab-11-iterable-dan-non-iterable/latihan/soal13.js
@@ -0,0 +1,12 @@
+const biodata = {
+  nama: "Andi",
+  umur: 20,
+  pekerjaan: "Programmer",
+  kota: "Yogyakarta",
+};
+
+console.log("====== BIODATA ======")
+for (let penyebut in biodata){
+        console.log(penyebut + "  :" + biodata[penyebut])
+}
+console.log("======================")
