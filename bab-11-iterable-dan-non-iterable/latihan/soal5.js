f7884daf58b373f0f6f3c5044aea8c821d6ef13c
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-07 10:33:29 +0700

    menghitung total nilai di dalam array dan rata rata

---
+++ bab-11-iterable-dan-non-iterable/latihan/soal5.js
@@ -0,0 +1,12 @@
+const nilai = [90, 80, 85, 100, 95];
+let jumlahNilai = 0
+
+for (let nilaiPelajaran of nilai){
+    jumlahNilai += nilaiPelajaran
+};
+
+
+let rataRata = jumlahNilai / nilai.length
+console.log("Jumlah nilai siswa     :",jumlahNilai)
+console.log("Rata-rata nilai siswa  :",rataRata)
+
