371b329ba3469f48b8174f5ad4be2b0aab28d7b1
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-10 13:28:12 +0700

    move string ke number

---
+++ bab-12-string-number-built-in-function/latihan/soal11.js
@@ -0,0 +1,7 @@
+const nilai1 = "85";
+const nilai2 = "90";
+
+// jikalau langsung di jumlah menggunakan:
+// console.log(nilai1 + nilai2) maka hasil akan 8590 karena nomur itu dalam bentuk string
+
+console.log("Total nilai :",Number(nilai1) + Number(nilai2))
