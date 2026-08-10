3c3941472045077d44b7d61b46a1d998291af620
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-10 14:06:22 +0700

    study case 2

---
+++ bab-12-string-number-built-in-function/latihan/studycase2.js
@@ -0,0 +1,11 @@
+const hargaBarang = "125000.50";
+const jumlah = "3";
+
+
+const harga = Number(hargaBarang)
+const jumlahNmr = Number(jumlah)
+const subtotal = harga + jumlahNmr
+
+console.log("Harga      :",harga)
+console.log("Jumlah     :",jumlahNmr)
+console.log("Subtotal   :",subtotal.toFixed())
