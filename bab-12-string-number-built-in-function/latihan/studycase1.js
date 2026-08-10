c627b418ca411f42c1291343ad2f6fc35d5fd126
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-10 13:59:09 +0700

    study case 1

--- bab-12-string-number-built-in-function/latihan/studycase1.js
+++ bab-12-string-number-built-in-function/latihan/studycase1.js
@@ -1,12 +1,16 @@
 const username = "   BUDI123   ";
 
+function validasi (username){
 const usernameBersih = username.trim()
 const usernameKecil = usernameBersih.toLowerCase()
 const validasiUser = usernameKecil
 console.log("Username :",validasiUser)
 if (validasiUser.length < 5){
-    console.log("Username Tidak Valid!")
+    console.log("Username Terlalu Pendek!")
 } 
 else{
     console.log("Username Valid!")
 }
+}
+
+validasi("  AB  ")
