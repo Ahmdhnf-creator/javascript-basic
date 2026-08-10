13073013b144044026adee40593d9e6b0b2fddb0
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-10 11:18:28 +0700

    incluedes

---
+++ bab-12-string-number-built-in-function/latihan/soal4.js
@@ -0,0 +1,8 @@
+const pesan = "Selamat datang di kelas JavaScript!";
+
+if (pesan.includes("JavaScript")){
+    console.log("Pesan membahas JavaScript")
+}
+else{
+    console.log("Pesan tidak membahas JavaScript")
+}
