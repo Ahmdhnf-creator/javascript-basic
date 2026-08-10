064a6beafd2680abc2e354942c5bdbdf1bd3c705
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-10 13:15:48 +0700

    replaceall

---
+++ bab-12-string-number-built-in-function/latihan/soal9.js
@@ -0,0 +1,5 @@
+const kalimat1 = "JavaScript itu seru. Saya suka JavaScript.";
+const kalimat2 = "JavaScript itu seru. Saya suka JavaScript.";
+
+console.log("Kalimat 1  :",kalimat1.replace("JavaScript","TypeScript"))
+console.log("Kalimat 2  :",kalimat2.replaceAll("JavaScript","TypeScript"))
