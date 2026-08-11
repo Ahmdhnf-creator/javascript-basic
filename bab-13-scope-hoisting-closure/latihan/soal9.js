59fae025f080fd3e5eeab1cca6a8391a4bf69df7
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-11 14:46:21 +0700

    Hoisting const

---
+++ bab-13-scope-hoisting-closure/latihan/soal9.js
@@ -0,0 +1,9 @@
+console.log(umur); // output eror dg alasan sama dg let
+
+const umur = 17;
+
+console.log(umur);
+
+var umur = 17;
+
+// perbedaan const dan var apabila dihoisting adalah, jika var berhasil di hoisting akan tetapi memiliki nilai undifaind sedangkan const juga sama berhasil dihoisting akan tetapi akan eror sebelum variabel tersebut dilewati
