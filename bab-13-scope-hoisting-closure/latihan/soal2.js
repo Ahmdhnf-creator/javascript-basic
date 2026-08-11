0318b4a716e57755a0f81a02f3442a5595555c02
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-11 10:33:04 +0700

    apakah bisa diakses

---
+++ bab-13-scope-hoisting-closure/latihan/soal2.js
@@ -0,0 +1,13 @@
+const nama = "Budi";
+
+function tampilkanNama() {
+  const umur = 17;
+
+  console.log(nama);
+  console.log(umur);
+}
+
+tampilkanNama();// output Budi dan 17
+
+console.log(nama);// output Budi
+console.log(umur);// output eror karena luar tidak bisa lihat dalam
