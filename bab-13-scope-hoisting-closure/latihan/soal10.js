749eef7eaf319cef85c1ae6c97b4df36f133fa88
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-11 14:49:08 +0700

    Tebak Output Hoisting

---
+++ bab-13-scope-hoisting-closure/latihan/soal10.js
@@ -0,0 +1,8 @@
+console.log(a);
+var a = 10;
+
+console.log(b);
+let b = 20;
+
+//console.log pertama berhasil akan tetapi nilainya kosong atau undifaind dikarenakan sistem tidak membaca nilainya, betul sudah dihoisting akan tetapi hanya deklarasinya 
+//ke dua , eror karena sistem tidak membaca sebelum melewati variabel tsb
