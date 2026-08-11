a6bedd68c41851811e6106743bb015972206e52e
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-11 10:28:46 +0700

    mengenal scope

---
+++ bab-13-scope-hoisting-closure/latihan/soal1.js
@@ -0,0 +1,14 @@
+const nama = "Budi";
+
+function belajar() {
+  const bahasa = "JavaScript";
+
+  if (true) {
+    let level = "Pemula";
+    console.log(level);
+  }
+}
+
+// nama    : global
+// bahasa  : local
+// level   : block
