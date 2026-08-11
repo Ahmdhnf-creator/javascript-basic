7c0fd5c9da51fd11cefb6d96fcd4e9fedcec6b7c
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-11 14:39:45 +0700

    Hoisting var

---
+++ bab-13-scope-hoisting-closure/latihan/soal7.js
@@ -0,0 +1,5 @@
+console.log(nama);//ini menghasilkan tidak eror karena hoisting pada var berhasil akan tetapi undifaind karena hoisting hanya mengangkan deklarasina aja bukan nilainya
+
+var nama = "Budi";
+
+console.log(nama);
