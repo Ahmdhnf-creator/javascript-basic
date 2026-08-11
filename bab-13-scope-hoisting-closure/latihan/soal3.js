ff94de5413039816b1067e2722da4062d6f01a62
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-11 10:36:41 +0700

    blockscope

---
+++ bab-13-scope-hoisting-closure/latihan/soal3.js
@@ -0,0 +1,9 @@
+if (true) {
+  let pesan = "Halo Santri";
+
+  console.log(pesan);
+}
+
+console.log(pesan);
+
+//program tidak bisa di jalankan hanya console.log pertama yang bisa dg output "Halo Santri", akan tetapi untuk console.log luar eror dikarenakan bagian luar tidak bisa melihat bagian dalam.
