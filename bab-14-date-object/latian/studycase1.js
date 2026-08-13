aee32d241215e02a275d362fb2c1f08765a833b3
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-13 11:39:10 +0700

    kejar deadline

--- bab-14-date-object/latian/studycase1.js
+++ bab-14-date-object/latian/studycase1.js
@@ -1,13 +1,26 @@
-const sekarang = new Date("2026-08-13");
+function sisaMenujuDeadline(deadline) {
+  const sekarang = new Date("2026-08-13");
+
+  const selisih =
+    (deadline.getTime() - sekarang.getTime()) / (1000 * 60 * 60 * 24);
+
+  if (selisih < 0) {
+    return "Deadline sudah terlewat.";
+  }
+
+  return selisih+" hari lagi."
+}
+
 const deadline = new Date("2026-08-20");
-const selisih = (deadline-sekarang)/(1000*60*60*24)
 
-
-console.log("Deadline       :",deadline.toLocaleDateString("id-ID",{
+console.log(
+  "Deadline:",
+  deadline.toLocaleDateString("id-ID", {
     weekday: "long",
     year: "numeric",
     month: "long",
-    day: "numeric"
-}))
+    day: "numeric",
+  }),
+);
 
-console.log("Sisa waktu     :",selisih,"hari lagi")
+console.log("Sisa waktu:", sisaMenujuDeadline(deadline));
