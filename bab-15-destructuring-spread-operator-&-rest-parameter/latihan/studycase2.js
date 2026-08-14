a70f2749b3c3393bd18648cbc11a4bee594cf7b0
Ahmdhnf <hanifabqari77@gmail.com>
2026-08-14 14:03:00 +0700

    Sistem Manajemen Tim

--- bab-15-destructuring-spread-operator-&-rest-parameter/latihan/studycase2.js
+++ bab-15-destructuring-spread-operator-&-rest-parameter/latihan/studycase2.js
@@ -5,7 +5,7 @@ const keranjangB = ["Monitor", "Headset"];
 const gabungan = [...keranjangA, ...keranjangB];
 console.log(gabungan);
 
-const [pertama,kedua,ketiga,terakhir]=gabungan
+const [pertama,kedua, ,terakhir]=gabungan
 console.log("Produk pertama     :",pertama);
 console.log("Produk kedua       :",kedua);
 console.log("Produk terakhir    :",terakhir);
